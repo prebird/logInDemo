4ef3d4eb4348a0919d790142a52a9075fad8685f
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-21 20:52:35 +0900

    로그인 성공 시, 쿠키에 멤버 ID 담기 -> 보안문제 있음

--- src/main/java/com/example/logindemo/web/login/LoginController.java
+++ src/main/java/com/example/logindemo/web/login/LoginController.java
@@ -2,6 +2,7 @@ package com.example.logindemo.web.login;
 
 import com.example.logindemo.domain.member.Member;
 import com.example.logindemo.service.LoginService;
+import com.example.logindemo.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -19,6 +22,7 @@ public class LoginController {
 
 
     private final LoginService logInService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
@@ -26,7 +30,9 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute("loginForm") LoginForm form,
+                        BindingResult bindingResult,
+                        HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -39,6 +45,9 @@ public class LoginController {
         }
 
         //TODO : 로그인 성공
+        // 쿠키에 로그인 성공한 유저 아이디 담기
+        Cookie cookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(cookie);
 
         return "redirect:/";
     }
