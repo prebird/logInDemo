c4d08b16ca0084cdd075277640a3d7875b23ec7e
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-25 22:50:59 +0900

    로그 인터셉터 구현

---
+++ src/main/java/com/example/logindemo/web/intercepter/LogInterceptor.java
@@ -0,0 +1,52 @@
+package com.example.logindemo.web.intercepter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        // @RequestMapping : HandlerMethod
+        // 정적 리소스 : ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handler1 = (HandlerMethod) handler; // 호출할 컨트롤러 메서드의 모든 정보가 포함되어 있음
+        }
+
+        log.info("Request [{}] [{}] [{}]", uuid, requestURI, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String)request.getAttribute(LOG_ID);
+        log.info("Response [{}] [{}] [{}]", logId, requestURI, handler);
+        
+        // 오류 로그
+        if (ex != null) {
+            log.error("afterCompletion error", ex);
+        }
+
+    }
+}
