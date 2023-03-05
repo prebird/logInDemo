8c3d9efa345118e0bdf2776c2f8a5026ca5ab6b8
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-06 00:20:52 +0900

    add BCryptPasswordEncoder to login edit loadUserByUsername to return UserDetails containing temp username, encoded password, authority

--- src/main/java/com/example/logindemo/service/security/CustomUserDetailsService.java
+++ src/main/java/com/example/logindemo/service/security/CustomUserDetailsService.java
@@ -1,10 +1,12 @@
-package com.example.logindemo.security;
+package com.example.logindemo.service.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -12,11 +14,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {   // call by login
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         log.info("loadUserByUsername : " + username);
 
-        return null;
+        // username, encoded password, authority
+        UserDetails userDetails = User.builder().username("user1")
+                .password(passwordEncoder.encode("111")) //TODO: Encode password
+                .authorities("ROLE_USER")
+                .build();
+
+        return userDetails;
     }
 }
--- src/main/java/com/example/logindemo/web/config/CustomSecurityConfig.java
+++ src/main/java/com/example/logindemo/web/config/CustomSecurityConfig.java
@@ -7,12 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
