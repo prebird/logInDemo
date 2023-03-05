3453630a7a817d0a6116054df4635e7ac15e3359
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-06 00:07:47 +0900

    ignore static files

--- src/main/java/com/example/logindemo/web/config/CustomSecurityConfig.java
+++ src/main/java/com/example/logindemo/web/config/CustomSecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.logindemo.web.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
@@ -17,5 +19,14 @@ public class CustomSecurityConfig {
         log.info("--------------- configure ---------------");
 
         return http.build();
+    }
+
+    // ignore filter on static files
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("-------------web configure--------------");
+
+        return (web) -> web.ignoring().requestMatchers(PathRequest.
+                                                        toStaticResources().atCommonLocations());
     }
 }
