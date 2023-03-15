9420ebdb46cc5eb80be4805af595dc5af9355441
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-15 23:04:55 +0900

    테스트 데이터 멤버에 암호 인코딩, 권한 추가

--- src/main/java/com/example/logindemo/TestDataInit.java
+++ src/main/java/com/example/logindemo/TestDataInit.java
@@ -4,8 +4,10 @@ import com.example.logindemo.domain.item.Item;
 import com.example.logindemo.domain.item.ItemRepository;
 import com.example.logindemo.domain.member.Member;
 import com.example.logindemo.domain.member.MemberRepository;
+import com.example.logindemo.domain.memberRole.MemberRole;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +18,8 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
     private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 테스트 데이터 추가
@@ -28,10 +32,14 @@ public class TestDataInit {
         itemRepository.save(new Item("감자칩", 1000, 20));
         itemRepository.save(new Item("빼빼로", 2000, 30));
 
-        Member member1 = memberRepository.save(new Member("admin", "12", "jeong"));
-        memberRepository.setAutoLoginId(member1.getId(), "84c247d3-ae86-4eee-95ab-bcce4567e598");
+        Member member1 = memberRepository.save(new Member("admin", passwordEncoder.encode("12"), "jeong"));
+        //memberRepository.setAutoLoginId(member1.getId(), "84c247d3-ae86-4eee-95ab-bcce4567e598");
+        member1.addRole(MemberRole.ADMIN);
+        Member member2 = new Member("user1", passwordEncoder.encode("123"), "koo");
+        member2.addRole(MemberRole.USER);
 
-        memberRepository.save(new Member("admin2", "12", "koo"));
+        memberRepository.save(member1);
+        memberRepository.save(member2);
         log.info("test data add");
     }
 }
